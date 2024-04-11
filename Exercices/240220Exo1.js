0badcb1c9c3a829e36984f8a2cf5c5b26662b2d2
Hyunji <hjkim96727@gmail.com>
2024-04-11 14:46:52 +0200

    first commit

---
+++ Exercices/240220Exo1.js
@@ -0,0 +1,11 @@
+//onsole.log("Hello");
+//console.log("Insert any message","You can write","various texts with comma");
+//alert("hello!");
+//confirm("are you more than 18 years old ?");
+
+/*alert() = confirm*/
+/*confirm() = confirm AND cancel*/ 
+
+
+let str = "Hello Java Script!";
+console.log(str);
