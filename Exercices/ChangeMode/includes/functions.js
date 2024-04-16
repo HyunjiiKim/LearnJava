e7b6c61198379ff027b5dbdecff7bb49c93817eb
Hyunji <hjkim96727@gmail.com>
2024-04-16 10:50:36 +0200

    Test : include html in html

--- Exercices/ChangeMode/includes/functions.js
+++ Exercices/ChangeMode/includes/functions.js
@@ -41,6 +41,10 @@ function modeHandler(self){
     }
 }
 
-function loadHtml(file){
-    $("").load("/includes/header.html")
+async function fetchHtmlAsText(url) {
+    return await (await fetch(url)).text();
+}
+
+async function importPage(target) {
+    document.querySelector('#' + target).innerHTML = await fetchHtmlAsText(target + '.html');
 }
